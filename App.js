902e5f1c63cc76ad9c8501204b241e22111145a9
TENXGIT <126549563+TENXGIT@users.noreply.github.com>
2023-06-04 00:22:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Countdowntimer from './components/count-down-timer/countdowntimer';
+
+function App() {
+  return (
+    <div className="App">
+<Countdowntimer/>
+    </div>
+  );
+}
+
+export default App;
